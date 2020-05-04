8b30eb65a19fc258265b4b73d688dce145dde647
jeanny <jleicaz@gmail.com>
2020-05-04 22:52:35 +0200

    Updates

---
+++ precache-manifest.5c47ae0ae4782b705bc9e38e5d1f6d8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9529110a0d0dc5c891d8a53242284c0f",
+    "url": "/gh-pages-test/index.html"
+  },
+  {
+    "revision": "db6549c5d177b500cc44",
+    "url": "/gh-pages-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f993b2317997b47a00af",
+    "url": "/gh-pages-test/static/js/2.d700a3df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-test/static/js/2.d700a3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6549c5d177b500cc44",
+    "url": "/gh-pages-test/static/js/main.738d19f7.chunk.js"
+  },
+  {
+    "revision": "8ca1805972bc3cf0ef01",
+    "url": "/gh-pages-test/static/js/runtime-main.bf720e5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
